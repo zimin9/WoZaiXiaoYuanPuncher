8ed6a100168ab650a13933c1b05d96e8cf5e14f6
PeterCai <i@zimings.com>
2021-07-28 22:00:23 +0800

    ❗ 修复在cron下执行脚本时，无法正确读取配置文件的bug

--- utils/configHandler.py
+++ utils/configHandler.py
@@ -4,7 +4,8 @@ import configparser
 
 class ConfigReader:
     def __init__(self):
-        root_dir = os.getcwd()  # 获取当前文件所在目录的上一级目录
+        root_dir = os.path.dirname(os.path.realpath('__file__'))  # 获取当前文件所在目录的上一级目录
+        print(root_dir)
         self.cf = configparser.ConfigParser()
         self.cf.read(root_dir + "/config.ini")  # 拼接得到config.ini文件的路径，直接使用
 
--- utils/jsonHandler.py
+++ utils/jsonHandler.py
@@ -6,7 +6,8 @@ from .configHandler import ConfigReader
 
 class Reader:
     def __init__(self):
-        self.path = os.getcwd()
+        self.path = os.path.dirname(os.path.realpath('__file__'))
+        print(self.path)
 
     def getJson(self, filename):
         with open(self.path + "/" + filename, 'r', encoding='utf8')as fp:
